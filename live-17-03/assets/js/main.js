90a5f4cb8ce6a17f44668e34a49ad91ea0ef5491
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-17 12:29:54 +0100

    cells live to color

---
+++ live-17-03/assets/js/main.js
@@ -0,0 +1,35 @@
+/* 
+Live Coding Minutella Grid dinamica:
+Creare in JavaScript una griglia 8x8.
+Ogni volta che clicco su un quadrato questo si colora di verde
+*/
+
+/* 
+Generare griglia con 64 celle 
+
+.seleziono il nodo della DOM dove inserire le 64 celle
+.ciclo 64 volte per generare le celle
+.inserisco le celle nel markup
+
+*/
+
+const containerElement = document.querySelector('.container');
+
+for (let i = 0; i < 64; i++) {
+   const cellMarkup = `<div class="cell"></div>`;
+
+   containerElement.insertAdjacentHTML('beforeend', cellMarkup);
+}
+
+const cellEL = document.querySelectorAll('.cell');
+
+for (let i = 0; i < cellEL.length; i++) {
+
+   const cell = cellEL[i];
+
+   cell.addEventListener('click', function (e) {
+      this.classList.toggle('bgGreen');
+   });
+}
+
+
