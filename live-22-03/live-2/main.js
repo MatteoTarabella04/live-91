8ff8dfb32afff3e4531dd43ebec87570f9392e5a
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-22 12:49:31 +0100

    live movies

---
+++ live-22-03/live-2/main.js
@@ -0,0 +1,43 @@
+
+
+const films = [
+
+   {
+      title: 'Star Wars',
+      duration: 130,
+      year: 2000,
+      img: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7ozBHvOoDDi4UcBWbz3N7SGOKdm.jpg'
+   },
+
+   {
+      title: 'The Equalizer',
+      duration: 125,
+      year: 2010,
+      img: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7xGr4gm4mu4J8BHyjEejfQCpwmJ.jpg'
+   },
+
+   {
+      title: 'Space Jam',
+      duration: 120,
+      year: 1998,
+      img: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/7Jd4M4vc6IvzL2sLDKkjWswF5rO.jpg'
+   }
+];
+
+const containerEl = document.querySelector('.container')
+console.log(films);
+
+for (let i = 0; i < films.length; i++) {
+   const thisMovie = films[i];
+   console.log(thisMovie);
+   const markupEl = `
+   <div>
+      <img src="${thisMovie.img}">
+      
+      <p>${thisMovie.title}</p>
+   </div>`;
+
+   containerEl.innerHTML += markupEl;
+   
+}
+
