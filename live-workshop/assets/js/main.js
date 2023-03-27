bd02677b13462e707c6a5fb4ec1d24ac5c7b7d35
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-27 11:52:53 +0200

    add markup + js + style

--- live-workshop/assets/js/main.js
+++ live-workshop/assets/js/main.js
@@ -108,18 +108,76 @@ class Product {
    //description = 'lorem ipsum'
    in_stock = false
 
-   constructor(name, description, price, category, likes = 100) {
+
+   /**
+    * 
+    * @param {String} name 
+    * @param {String} description 
+    * @param {Float} price 
+    * @param {String} category 
+    * @param {String} img_path 
+    * @param {Number} likes 
+    */
+   constructor(name, description, price, category, img_path, likes = 100) {
       this.name = name
+      this.description = description
       this.price = price
       this.category = category
+      this.img_path = img_path
       this.likes = likes
+   }
+
+   addLikes() {
+      this.likes++
    }
 
 }
 
 
-const ssd = new Product('SSD 1TB', 'Super fast ssd drive', 60, 'Pc hardware', 120)
+/* const ssd = new Product('SSD 1TB', 'Super fast ssd drive', 60, 'Pc hardware', 120)
 const monitor = new Product('AOC Monitor', 'Studio monitor', 149.99, 'Monitors / TV')
 
 console.log(ssd);
 console.log(monitor);
+
+ssd.addLikes()
+console.log(ssd); */
+
+
+const Products = [
+   new Product('SSD 1TB Crucial Disk', 'Super fast SSD drive', 49.99, 'PC Hardware', './assets/img/ssd.jpg'),
+   new Product('Aoc 24inc Monitor', 'ultra thin monitor', 149.99, 'PC Monitors', './assets/img/monitor.jpg'),
+   new Product('Corsair ATX Case', 'Small form factor atx desktop pc case', 99.99, 'PC Accessories', './assets/img/case.jpg'),
+   new Product('Anker Vertical mouse', 'Comfy vertical mouse', 29.99, 'PC accessories', './assets/img/mouse.jpg'),
+   new Product('Lenovo Idea pad', 'Modern 8gb ram ultra thin laptop', 549.99, 'Laptops', './assets/img/laptop.webp'),
+   new Product('Walking desk', 'Get fit with the best walking desk', 649.99, 'Office equipment', './assets/img/desk.webp'),
+]
+
+console.log(Products);
+
+/*  generate cards */
+const prodElemenst = document.getElementById('products')
+
+Products.forEach(prod => {
+   const markup = `
+   <div class="product">
+      <div class="price">${prod.price}</div>
+         <img src="${prod.img_path}" alt="${prod.name}">
+         <h3>${prod.name}</h3>
+         <p>${prod.description}</p>
+         <button data-product-price="${prod.price}" data-product-name="${prod.name}">Buy Now</button>
+   </div>
+   `
+   prodElemenst.insertAdjacentHTML('beforeend', markup)
+})
+
+const btnElements = document.querySelectorAll('.product button');
+
+btnElements.forEach(btn => {
+   btn.addEventListener('click', () => {
+      console.log(btn);
+   })
+})
+
+
+
