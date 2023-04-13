58c3bf062eccba6a32d84869d69f5f46149e7324
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-04-13 13:10:08 +0200

    live 13-04

---
+++ live-vite-13-04/src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import products from "./data/products.js";
+export default {
+  data() {
+    return {
+      products: products,
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col" v-for="product in products">
+        <div class="product">
+          <img :src="product.src" :alt="product.title">
+          {{ product.title }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
